5648be7636265400f66a36e21988cf6d5ebcab06
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2025-04-29 01:58:27 +0600

    added task02

---
+++ Assignment_06/task02.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class task02
+{
+  public static void main(String[] args)
+ {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Enter string : ");
+    
+    String inputtedString = sc.nextLine();
+    
+    int i = inputtedString.length() - 1;
+    
+    while(i > -1)
+    {
+      
+      char characterOfInputtedString = inputtedString.charAt( i );
+      
+      System.out.print( characterOfInputtedString );
+      i--;
+    
+    }
+  }
+}
