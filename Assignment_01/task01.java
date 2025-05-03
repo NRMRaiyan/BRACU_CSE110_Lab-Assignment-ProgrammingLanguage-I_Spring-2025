ec0bfd1e03f4baa24fc698b67a91c025b0d7d005
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2025-05-04 02:49:58 +0600

    added task01

---
+++ Assignment_01/task01.java
@@ -0,0 +1,15 @@
+public class main{
+  public static void task01(String[] args){
+    int hungry; // correct
+    int 2AB; // incorrect - cannot start with numbers
+    int 312.2; // incorrect - cannot start with numbers
+    int MOBILE; // correct
+    int "Ans"; // incorrect - cannot start special characters
+    int $30; // correct
+    int Yes/No; // incorrect - special character
+    int student-id; // incorrect - special character
+    int  A+3; // incorrect - special character
+    int ‘X’; // incorrect - special character
+    int return; // incorrect - cannot give any keyword name
+  }
+}
