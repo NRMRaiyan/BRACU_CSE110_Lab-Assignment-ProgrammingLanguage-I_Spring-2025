017d0925e41f67ef67fbf247f4adf81c07709659
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2025-05-04 02:53:27 +0600

    Update task01.java

--- Assignment_02/task01.java
+++ Assignment_02/task01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-public class main{
-  public static void task01 (String [] args){
+public class task01{
+  public static void main (String [] args){
     
     Scanner sc = new Scanner(System.in);
     
