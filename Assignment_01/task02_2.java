f3fa2c26bd96481aab401514f7045814178f314c
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2025-05-06 01:34:22 +0600

    added task02_2

---
+++ Assignment_01/task02_2.java
@@ -0,0 +1,11 @@
+public class task02_2{
+  public static void main(String[] args){
+
+    int number1 = 15;
+    int number2 = 56;
+
+    int sumNumbers = number1 + number2;
+
+    System.out.println("The sum of number1 and number2 is : " + sumNumbers);
+  }
+}
