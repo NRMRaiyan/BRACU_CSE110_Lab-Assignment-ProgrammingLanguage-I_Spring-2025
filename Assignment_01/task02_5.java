ae13688456552562e73676f644ecc4b7c07e600e
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2025-05-06 01:38:22 +0600

    added task02_5

---
+++ Assignment_01/task02_5.java
@@ -0,0 +1,23 @@
+public class task02_5{
+  public static void main(String[] args){
+    
+    double number = 15.63d;
+    System.out.println("The number is : " + number);
+    
+    int number1 = 15;
+    double number2 = 56.95d;
+    
+    double sumNumbers = number1 + number2;
+    
+    System.out.println("The sum of number1 and number2  is : " + sumNumbers);
+
+    int numberX = 90;
+    double numberY = 5.5d;
+    
+    double productNumbers = numberX * numberY;
+    double divideNumbers = numberX / numberY;
+    
+    System.out.println("The product of numberX and numberY is : " + productNumbers);
+    System.out.println("The division of numberX and numberY is : " + divideNumbers);
+  }
+}
