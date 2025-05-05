c413e3995b85dcbb97f06981ed26edd9281af2d0
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2025-05-06 01:36:00 +0600

    added task02_3

---
+++ Assignment_01/task02_3.java
@@ -0,0 +1,13 @@
+public class task02_3{
+  public static void main(String[] args){
+
+    int number1 = 90;
+    int number2 = 5;
+
+    int productNumbers = number1 * number2;
+    int divideNumbers = number1 / number2;
+
+    System.out.println("The product of number1 and number2 is : " + productNumbers);
+    System.out.println("The division of number1 and number2 is : " + divideNumbers);
+  }
+}
