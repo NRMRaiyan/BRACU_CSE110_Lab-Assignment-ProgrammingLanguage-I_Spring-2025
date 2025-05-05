27fbb62cca0e7f5c4e8cf838904443448e940482
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2025-05-06 01:39:43 +0600

    added task02_6

---
+++ Assignment_01/task02_6.java
@@ -0,0 +1,18 @@
+public class task02_6{
+  public static void main(String[] args){
+    
+    String firstName = "Nafizur";
+    System.out.println(firstName);
+    
+    String middleName = "Rahman";
+    String lastName = "Mallick";
+    String addNames = firstName + middleName + lastName;
+    System.out.println(addNames);
+  }
+}
+
+#How does the addition operator work for Strings?
+=> The addition operator work for strings are like what is stored inside the strings will just add beside each other.
+
+#What if the first variable is an integer and the second is a String and vice versa?
+=> In this step the integer will print first and beside that the string will be printed as it will be stored in variable.
