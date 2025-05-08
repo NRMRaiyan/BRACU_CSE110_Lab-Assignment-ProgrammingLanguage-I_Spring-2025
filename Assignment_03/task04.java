89913386125abd1a451ba08dd1ac6d083e58bb0e
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2025-05-08 14:58:11 +0600

    added task04

---
+++ Assignment_03/task04.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class task04{
+  public static void main(String [] args){
+     
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Enter number that will be added upto: ");
+    int number = sc.nextInt();
+    
+    int sum = 0;
+    for(int i = 1; i <= number; i++){
+      if(i % 7 == 0 || i % 9 == 0){
+        sum = sum + i;
+      }
+    }
+    System.out.println("The sum: " + sum);
+  }
+}
