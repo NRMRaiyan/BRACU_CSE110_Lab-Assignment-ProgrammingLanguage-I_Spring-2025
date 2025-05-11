878ba1d840105accb90f6b01078c069894a0a59f
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2025-05-12 00:29:09 +0600

    added task01_B

---
+++ Assignment_09/task01_B.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class task01_B{
+  
+  // Task01_A
+  public static void oneToN(int number1, int number2){
+    if(number1 > number2) return;
+    
+    System.out.print(number1 + " ");
+    
+    oneToN(number1 + 1, number2);
+  }
+  
+  // Task01_B
+  public static void nToOne(int number1, int number2){
+    if(number1 < number2) return;
+    
+    System.out.print(number1 + " ");
+    
+    nToOne(number1 - 1, number2);
+  }
+  
+  public static void main(String[] args){
+    
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Enter value of N : ");
+    int numberN = sc.nextInt();
+    
+    oneToN(1, numberN);
+    
+    System.out.println();
+    
+    nToOne(numberN, 1);
+  }
+}
