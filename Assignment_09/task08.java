aac9fcd500c8fa009f9c72f3ad969cf47034476d
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2025-09-11 13:45:40 +0600

    Create task08.java

---
+++ Assignment_09/task08.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class task08{
+  
+  // Task 08
+	  public static int fibonacci(int number)
+{
+  
+  if(number == 0) return 0;
+
+    else if(number == 1) return 1;
+    
+    return fibonacci(number - 1) + fibonacci(number - 2);
+
+  }
+  
+  	public static void main(String[] args)
+{
+    
+    Scanner sc = new Scanner(System.in);
+    
+   	System.out.println("Enter number : ");
+
+    int number = sc.nextInt();
+    
+    int finalAnswer = fibonacci(number);
+
+   	System.out.println(finalAnswer);
+  }
+}
