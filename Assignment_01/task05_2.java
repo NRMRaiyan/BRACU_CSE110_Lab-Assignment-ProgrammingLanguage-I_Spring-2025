1413a129e9b63104c59360c74fad769cb05cdd79
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2025-09-13 00:43:27 +0600

    Create task05_2.java

---
+++ Assignment_01/task05_2.java
@@ -0,0 +1,14 @@
+public class main{
+  public static void main(String[] args){
+    
+    int number1 = 36;
+    int number2 = 71;
+    
+    number1 = number1 * number2;
+    number2= number1 / number2;
+    number1 = number1 / number2;
+    
+    System.out.println("After swapping number1 = " + number1 + " and number2 = " + number2);
+
+  }
+}
