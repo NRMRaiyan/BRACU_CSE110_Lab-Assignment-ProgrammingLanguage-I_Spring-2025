3fecc0b69e941b6ee257b8ecf22e7c5508d1d8fb
Nafizur Rahman Mallick <114010735+NRMRaiyan@users.noreply.github.com>
2025-09-13 00:42:15 +0600

    Create task03.java

---
+++ Assignment_01/task03.java
@@ -0,0 +1,13 @@
+public class main{
+  public static void main(String[] args){
+    
+    float radius = 4f;
+    
+    double circumference = 2 * Math.PI * radius;
+    double area = Math.PI * radius * radius;
+    
+    System.out.println("The circumference of the circle is : " + circumference);
+    System.out.println("The area of the circle is : " + area);
+
+  }
+}
